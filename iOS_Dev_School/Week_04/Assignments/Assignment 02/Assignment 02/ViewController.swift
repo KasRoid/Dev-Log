705c822bdbe8db1c8cfb92196570be752092ebb7
KasRoid <KasRoid@gmail.com>
2020-04-28 19:27:31 +0900

    Assignment DONE

--- iOS_Dev_School/Week_04/Assignments/Assignment 02/Assignment 02/ViewController.swift
+++ iOS_Dev_School/Week_04/Assignments/Assignment 02/Assignment 02/ViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
     
     var textInput = UITextField()
     var resultLabel = UILabel()
@@ -16,14 +16,21 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        textInput.frame = CGRect(x: 50, y: 400, width: 250, height: 50)
+        textInput.frame = CGRect(x: 40, y: 400, width: 300, height: 50)
         textInput.borderStyle = .roundedRect
+        textInput.textAlignment = .center
+        textInput.delegate = self
+        textInput.placeholder = "Enter your text here"
         view.addSubview(textInput)
         textInput.addTarget(self, action: #selector(textFieldDidBeginEditing(_:)), for: UIControl.Event.editingDidBegin)
         textInput.addTarget(self, action: #selector(textFieldDidChanged(_:)), for: UIControl.Event.editingChanged)
+        textInput.addTarget(self, action: #selector(textFieldDidEndOnExit(_:)), for: UIControl.Event.editingDidEndOnExit)
+        textInput.addTarget(self, action: #selector(textFieldEditingDidEnd(_:)), for: UIControl.Event.editingDidEnd)
         
-        resultLabel.frame = CGRect(x: 50, y: 200, width: 250, height: 50)
-        resultLabel.backgroundColor = .lightGray
+        resultLabel.frame = CGRect(x: 40, y: 200, width: 300, height: 50)
+        resultLabel.layer.borderColor = UIColor.lightGray.cgColor
+        resultLabel.layer.borderWidth = 2.0
+        resultLabel.textAlignment = .center
         view.addSubview(resultLabel)
     }
     
@@ -33,11 +40,24 @@ class ViewController: UIViewController {
     }
     
     @objc func textFieldDidChanged(_ textField: UITextField) {
-        resultLabel.text = textInput.text
+                resultLabel.text = textInput.text
+    }
+    
+    @objc func textFieldDidEndOnExit(_ textField: UITextField) {
+        
     }
     
     @objc func textFieldEditingDidEnd(_ textField: UITextField) {
         resultLabel.textColor = .systemRed
         resultLabel.font = UIFont.systemFont(ofSize: 20)
     }
+    
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+      let maxLength = 10
+      let currentString: NSString = textField.text! as NSString
+      let newString: NSString =
+        currentString.replacingCharacters(in: range, with: string) as NSString
+      return newString.length <= maxLength
+    }
 }
+
