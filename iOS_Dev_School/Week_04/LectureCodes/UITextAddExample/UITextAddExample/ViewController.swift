ce10a2058d5f871bb53811b4365c7d52aaca76d3
KasRoid <KasRoid@gmail.com>
2020-04-28 13:05:15 +0900

    update

---
+++ iOS_Dev_School/Week_04/LectureCodes/UITextAddExample/UITextAddExample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UITextAddExample
+//
+//  Created by Kas Song on 2020.04.28.
+//  Copyright © 2020 Kas Song. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ iOS_Dev_School/Week_04/LectureCodes/UIViewController_Self_Practice/UIViewController_Self_Practice/SceneDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  SceneDelegate.swift
+//  UIViewController_Self_Practice
+//
+//  Created by Kas Song on 2020.04.28.
+//  Copyright © 2020 Kas Song. All rights reserved.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+                
+        guard let _ = (scene as? UIWindowScene) else { return }
+    }
+}
