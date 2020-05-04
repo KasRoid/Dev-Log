0e7c305b99b7beb3e38d92ab2c5ce20e8147835f
KasRoid <KasRoid@gmail.com>
2020-05-04 13:16:11 +0900

    Exam01 Solution Update

---
+++ iOS_Dev_School/Week_04/Exam 01/FirstTest/FirstTest/AppDelegate.swift
@@ -0,0 +1,16 @@
+//
+//  AppDelegate.swift
+//  FirstTest
+//
+//  Created by Giftbot on 2020/05/04.
+//  Copyright © 2020 giftbot. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+final class AppDelegate: UIResponder, UIApplicationDelegate {
+
+  var window: UIWindow?
+
+}
