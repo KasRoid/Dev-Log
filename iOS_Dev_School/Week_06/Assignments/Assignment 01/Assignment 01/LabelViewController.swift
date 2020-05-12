de52106541fdf610e524ed295f90980c37cbdd61
KasRoid <KasRoid@gmail.com>
2020-05-12 23:53:02 +0900

    Assignment 01 DONE

---
+++ iOS_Dev_School/Week_06/Assignments/Assignment 01/Assignment 01/LabelViewController.swift
@@ -0,0 +1,46 @@
+//
+//  LabelViewController.swift
+//  Assignment 01
+//
+//  Created by Kas Song on 2020.05.12.
+//  Copyright © 2020 Kas Song. All rights reserved.
+//
+
+import UIKit
+
+class LabelViewController: UIViewController, CustomViewDelegate {
+    
+    var resultLabel: UILabel = {
+        var label = UILabel()
+        label.text = "My Label"
+        label.textAlignment = .center
+        label.frame.size = CGSize(width: 250, height: 40)
+        return label
+    }()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let viewController = presentingViewController as! ViewController
+        viewController.delegate = self
+        view.backgroundColor = .systemBackground
+        updateUI()
+    }
+    
+//    override func viewWillAppear(_ animated: Bool) {
+//        let viewController = ViewController()
+//        viewController.delegate = self
+//    }
+    
+    func updateUI() {
+        resultLabel.frame.origin = CGPoint(x: view.frame.midX - 175, y: view.frame.midY - 20)
+        resultLabel.layer.borderColor = UIColor.lightGray.cgColor // border 색 설정
+        resultLabel.layer.borderWidth = 2.0
+        view.addSubview(resultLabel)
+    }
+    
+    func sendText(data: String) {
+        resultLabel.text = data
+    }
+
+}
---
+++ iOS_Dev_School/Week_06/Assignments/Assignment 01/Assignment 01/ViewController.swift
@@ -0,0 +1,60 @@
+//
+//  ViewController.swift
+//  Assignment 01
+//
+//  Created by Kas Song on 2020.05.12.
+//  Copyright © 2020 Kas Song. All rights reserved.
+//
+
+import UIKit
+
+protocol CustomViewDelegate: class {
+    func sendText(data: String)
+}
+
+class ViewController: UIViewController {
+    
+    weak var delegate: CustomViewDelegate?
+    
+    var textInput: UITextField = {
+        let text = UITextField()
+        text.placeholder = "Enter your text"
+        text.borderStyle = .roundedRect
+        text.frame.size = CGSize(width: 250, height: 40)
+        return text
+    }()
+    var enterBtn: UIButton = {
+        var button = UIButton(type: .system)
+        button.setTitle("Enter", for: .normal)
+        button.setTitleColor(.white, for: .normal)
+        button.layer.cornerRadius = 5
+        button.backgroundColor = .gray
+        button.frame.size = CGSize(width: 80, height: 40)
+        button.addTarget(self, action: #selector(buttonPressed(_:)), for: .touchUpInside)
+        return button
+    }()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        updateUI()
+    }
+    
+    func updateUI() {
+        textInput.frame.origin = CGPoint(x: view.frame.midX - 125, y: view.frame.midY - 80)
+        view.addSubview(textInput)
+        
+        enterBtn.frame.origin = CGPoint(x: view.frame.midX - 45, y: view.frame.midY)
+        view.addSubview(enterBtn)
+    }
+    
+    @objc func buttonPressed(_ sender: UIButton) {
+        let anotherVC = LabelViewController()
+        anotherVC.modalPresentationStyle = .automatic
+        if let data = textInput.text {
+            present(anotherVC, animated: true, completion: {self.delegate?.sendText(data: data)})
+        }
+    }
+}
+
+//            self.delegate = anotherVC
