062cff220904f24bc022fe5327a946896262a926
KasRoid <KasRoid@gmail.com>
2020-05-27 20:53:51 +0900

    Assignment update

--- iOS_Dev_School/Week_08/Assignments/ShoppingItems - Starter/ShoppingItems/ItemCell.swift
+++ iOS_Dev_School/Week_08/Assignments/ShoppingItems - Starter/ShoppingItems/ItemCell.swift
@@ -10,49 +10,27 @@ import UIKit
 
 
 final class ItemCell: UITableViewCell {
+    weak var delegate: ItemCellDelegate?
     
     var btnCount = UIButton(type: .contactAdd)
     var countLabel = UILabel()
     
-    let itemTitle = ["iPhone7", "iPhone8", "iPhoneSE_Gold", "iPhoneSE_RoseGold", "iPhoneX_SpaceGray", "iPhoneX_White", "iPhoneXs_SpaceGray", "iPhoneXs_White", "iPhoneSE2", "iPhone11_SpaceGray", "iPhone11_White", "iPhone11Pro_SpaceGray", "iPhone11Pro_White"]
-    let itemsInStock = [
-        "iPhone7": ["iPhone8", "10"],
-        "iPhone8": ["iPhone8", "10"],
-        "iPhoneSE_Gold": ["iPhoneSE_Gold", "8"],
-        "iPhoneSE_RoseGold": ["iPhoneSE_RoseGold", "8"],
-        "iPhoneX_SpaceGray": ["iPhoneX_SpaceGray", "5"],
-        "iPhoneX_White": ["iPhoneX_White", "5"],
-        "iPhoneXs_SpaceGray": ["iPhoneX_SpaceGray", "5"],
-        "iPhoneXs_White": ["iPhoneX_White", "5"],
-        "iPhoneSE2": ["iPhone8", "10"],
-        "iPhone11_SpaceGray": ["iPhoneX_SpaceGray", "5"],
-        "iPhone11_White": ["iPhoneX_White", "5"],
-        "iPhone11Pro_SpaceGray": ["iPhoneX_SpaceGray", "5"],
-        "iPhone11Pro_White": ["iPhoneX_White", "5"]
-    ]
-    var itemCount: Array<Int> = []
-    
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
-        
-        if itemCount.count == 0 {
-            print(itemCount.count)
-            itemCount = []
-            for _ in 1...itemTitle.count {
-                itemCount.append(0)
-            }
-            print(itemCount)
-        }
         
         btnCount.addTarget(self, action: #selector(buttonPressed(_:)), for: .touchUpInside)
         contentView.addSubview(btnCount)
         contentView.addSubview(countLabel)
-        print(#function)
     }
     
     required init?(coder: NSCoder) {
         super.init(coder: coder)
 //        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        
     }
     
     override func layoutSubviews() {
@@ -62,8 +40,10 @@ final class ItemCell: UITableViewCell {
     }
     
     @objc func buttonPressed(_ sender: UIButton) {
-        itemCount[sender.tag] += 1
-        countLabel.text = String(itemCount[sender.tag]) // 질문
-        print(itemCount)
+        self.delegate?.sendButtonTag(sender.tag)
     }
+}
+
+protocol ItemCellDelegate: class {
+    func sendButtonTag(_ data: Int)
 }
--- iOS_Dev_School/Week_08/Assignments/ShoppingItems - Starter/ShoppingItems/ViewController.swift
+++ iOS_Dev_School/Week_08/Assignments/ShoppingItems - Starter/ShoppingItems/ViewController.swift
@@ -12,19 +12,34 @@ final class ViewController: UIViewController {
     
     @IBOutlet private weak var tableView: UITableView!
     
+    var itemTitle: Array<String> = []
+    var itemsInStock = [
+        "iPhone7": ["iPhone8", "0", "10"],
+        "iPhone8": ["iPhone8", "0", "10"],
+        "iPhoneSE_Gold": ["iPhoneSE_Gold", "0", "8"],
+        "iPhoneSE_RoseGold": ["iPhoneSE_RoseGold", "0", "8"],
+        "iPhoneX_SpaceGray": ["iPhoneX_SpaceGray", "0", "5"],
+        "iPhoneX_White": ["iPhoneX_White", "0", "5"],
+        "iPhoneXs_SpaceGray": ["iPhoneX_SpaceGray", "0", "5"],
+        "iPhoneXs_White": ["iPhoneX_White", "0", "5"],
+        "iPhoneSE2": ["iPhone8", "0", "10"],
+        "iPhone11_SpaceGray": ["iPhoneX_SpaceGray", "0", "5"],
+        "iPhone11_White": ["iPhoneX_White", "0", "5"],
+        "iPhone11Pro_SpaceGray": ["iPhoneX_SpaceGray", "0", "5"],
+        "iPhone11Pro_White": ["iPhoneX_White", "0", "5"]
+    ]
+    var buttonTag = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         setupUI()
     }
     
     private func setupUI() {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "ItemCell") as! ItemCell
-        for _ in 1...cell.itemTitle.count {
-            cell.itemCount.append(0)
+        for keys in itemsInStock.keys {
+            itemTitle.append(keys)
         }
-        
         tableView.dataSource = self
-        tableView.delegate = self
         tableView.register(ItemCell.self, forCellReuseIdentifier: "ItemCell")
         tableView.frame = view.frame
         tableView.rowHeight = 80
@@ -35,30 +50,50 @@ final class ViewController: UIViewController {
 // MARK: - UITableViewDataSource
 
 extension ViewController: UITableViewDataSource {
+    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "ItemCell") as! ItemCell
-        let numberOfProducts = cell.itemTitle.count
-        return numberOfProducts
+        return itemTitle.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "ItemCell", for: indexPath) as! ItemCell
+        cell.delegate = self
+        let nameOfProducts = itemTitle[indexPath.row] // 제품명
+        let imageOfProducts = itemsInStock[nameOfProducts]![0]
+        let quantityOfProducts = String(itemsInStock[nameOfProducts]![1])
         cell.btnCount.tag = indexPath.row
         cell.countLabel.tag = indexPath.row
+        cell.countLabel.text = itemsInStock[nameOfProducts]![1]
         
-        let products = cell.itemsInStock
-        let productsDetail = "\(products[cell.itemTitle[indexPath.row]]![0])"
-        cell.imageView?.image = UIImage(named: productsDetail)
-        cell.textLabel?.text = cell.itemTitle[indexPath.row]
-        cell.countLabel.text = String(cell.itemCount[indexPath.row])
+        cell.imageView?.image = UIImage(named: imageOfProducts)
+        cell.textLabel?.text = nameOfProducts
+        cell.countLabel.text = quantityOfProducts
         return cell
     }
 }
 
-extension ViewController: UITableViewDelegate {
-    func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) {
-//        let cell = tableView.dequeueReusableCell(withIdentifier: "ItemCell", for: indexPath) as! ItemCell
-//        cell.countLabel.text = String(cell.itemCount[indexPath.row])
+extension ViewController: ItemCellDelegate {
+    func sendButtonTag(_ data: Int) {
+        buttonTag = data
+        var quantityOfSelectedItem = Int(itemsInStock[itemTitle[buttonTag]]![1])! // 버튼 누른 제품의 갯수를 Int 로 변환하여 변수에 할당
+        let maxQuantityOfSelectedItem = Int(itemsInStock[itemTitle[buttonTag]]![2])!
+        if quantityOfSelectedItem < maxQuantityOfSelectedItem {
+            quantityOfSelectedItem += 1
+            itemsInStock[itemTitle[buttonTag]]![1] = String(quantityOfSelectedItem) // 딕셔너리값 수정
+        }
+        else {
+            let indexPath: IndexPath = IndexPath(row: buttonTag, section: 0)
+            let cell = tableView.cellForRow(at: indexPath)!
+            UITableViewCell.animate(withDuration: 0.5, animations: { cell.backgroundColor = .red }) { _ in
+                UITableView.animate(withDuration: 0.5) { cell.backgroundColor = .clear }
+            }
+
+            let alertController = UIAlertController(title: "수량 초과", message: "구매할 수 있는 최대수량을 초과하였습니다.", preferredStyle: .alert)
+            let confirmAction = UIAlertAction(title: "확인", style: .cancel, handler: nil)
+            alertController.addAction(confirmAction)
+            present(alertController, animated: true)
+        }
+        print(itemsInStock[itemTitle[buttonTag]]![1])
+        tableView.reloadData()
     }
 }
-
